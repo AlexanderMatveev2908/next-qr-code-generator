e1fb0f8f226c9c146857005fba8d5d3ab3d4a537
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-21 13:10:21 +0300

    template_home => build header

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,27 +1,9 @@
 "use client";
 
-import AppLogo from "@/common/components/elements/AppLogo";
-import { css } from "@emotion/react";
 import { type FC } from "react";
 
 const Home: FC = () => {
-  return (
-    <main className="w-full h-full flex flex-col">
-      <header className="w-[90%] sm:w-[75%] lg:w-[calc(100%-288px)] mx-auto">
-        <div className="w-full flex items-center justify-start gap-[12xp]">
-          <AppLogo
-            {...{
-              $customCSS: css`
-                width: 40px;
-                height: 40px;
-                background: var(--purple__pmr);
-              `,
-            }}
-          />
-        </div>
-      </header>
-    </main>
-  );
+  return <main className="w-full h-full flex flex-col"></main>;
 };
 
 export default Home;
