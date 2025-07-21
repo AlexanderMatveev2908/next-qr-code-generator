d98785cdeb5ffdb48102a52264b4ed8211645e1f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-21 15:11:35 +0300

    template_home => added custom hook

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import Hero from "@/features/home/components/Hero";
 import { type FC } from "react";
 
 const Home: FC = () => {
-  return <main className="w-full h-full flex flex-col"></main>;
+  return (
+    <main className="w-full h-full flex flex-col">
+      <Hero />
+    </main>
+  );
 };
 
 export default Home;
---
+++ apps/client/src/features/home/components/Hero.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+"use client";
+
+import type { FC } from "react";
+
+const Hero: FC = () => {
+  return <div></div>;
+};
+
+export default Hero;
