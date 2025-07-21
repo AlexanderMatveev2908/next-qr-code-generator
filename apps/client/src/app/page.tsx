b0d9f9803b59c14107dd2a478277658233170631
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-21 13:04:13 +0300

    template_home => created logo component

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,5 +1,7 @@
-import { InnerLogoSvg } from "@/common/components/SVGs";
-import { genIps } from "@shared/first/lib/etc.js";
+"use client";
+
+import AppLogo from "@/common/components/elements/AppLogo";
+import { css } from "@emotion/react";
 import { type FC } from "react";
 
 const Home: FC = () => {
@@ -7,9 +9,15 @@ const Home: FC = () => {
     <main className="w-full h-full flex flex-col">
       <header className="w-[90%] sm:w-[75%] lg:w-[calc(100%-288px)] mx-auto">
         <div className="w-full flex items-center justify-start gap-[12xp]">
-          <div className="w-[40px] h-[40px] bg-[var(--purple__pmr)] flex justify-center items-center rounded-[7.5px]">
-            <InnerLogoSvg className="w-[37.5%] h-[45%] text-[var(--white__sec)]" />
-          </div>
+          <AppLogo
+            {...{
+              $customCSS: css`
+                width: 40px;
+                height: 40px;
+                background: var(--purple__pmr);
+              `,
+            }}
+          />
         </div>
       </header>
     </main>
---
+++ apps/client/src/common/components/elements/AppLogo.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource @emotion/react */
+"use client";
+
+import type { FC } from "react";
+import { InnerLogoSvg } from "../SVGs";
+import { css, SerializedStyles } from "@emotion/react";
+
+type PropsType = {
+  $customCSS: SerializedStyles;
+};
+
+const AppLogo: FC<PropsType> = ({ $customCSS }) => {
+  return (
+    <div
+      css={css`
+        ${$customCSS}
+      `}
+      className="flex justify-center items-center rounded-[20%]"
+    >
+      <InnerLogoSvg className="w-[37.5%] h-[45%] text-[var(--white__sec)]" />
+    </div>
+  );
+};
+
+export default AppLogo;
