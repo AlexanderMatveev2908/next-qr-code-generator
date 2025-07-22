f33ffb3377aa234e8b13720eb5ac1cfbf546d8f2
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-22 19:12:59 +0300

    route_server_post => made logger

--- apps/server/src/features/qr/router.ts
+++ apps/server/src/features/qr/router.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { genQrCtrl } from "./controllers/post.js";
 import { wrapAPI } from "@src/middleware/wrapAPI.js";
+import { logJSON } from "@src/middleware/logJSON.js";
 
 export const routerQr = async (app: FastifyInstance) => {
   app.route({
     url: "/",
     method: "POST",
-    preHandler: [],
+    preHandler: [logJSON],
     handler: wrapAPI(genQrCtrl),
   });
 };
