29b8ad95eeb3fbd767a42223f921553217abf2cb
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-23 11:40:01 +0300

    made api route

--- apps/server/src/features/qr/router.ts
+++ apps/server/src/features/qr/router.ts
@@ -5,11 +5,20 @@ import { logJSON } from "@src/middleware/logJSON.js";
 import { checkPostQr } from "./middleware/checkPostQr.js";
 import { parseQuery } from "@src/middleware/parseQuery.js";
 
+const commonMdw = [parseQuery, logJSON, checkPostQr];
+
 export const routerQr = async (app: FastifyInstance) => {
   app.route({
     url: "/",
     method: "POST",
-    preHandler: [parseQuery, logJSON, checkPostQr],
+    preHandler: commonMdw,
+    handler: wrapAPI(genQrCtrl),
+  });
+
+  app.route({
+    url: "/",
+    method: "GET",
+    preHandler: commonMdw,
     handler: wrapAPI(genQrCtrl),
   });
 };
