e84c3a4baed490c2e7d4be8aacdcf32e24141c67
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-23 10:58:40 +0300

    _

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { envApp } from "@/core/constants/env";
 import Hero from "@/features/home/components/Hero";
 import HomeFooter from "@/features/home/components/HomeFooter/HomeFooter";
+import { __cg } from "@shared/first/lib/logger.js";
 import { type FC } from "react";
 
 const Home: FC = () => {
+  __cg("env", envApp);
+
   return (
     <main className="w-full min-h-screen h-full flex flex-col">
       <Hero />
