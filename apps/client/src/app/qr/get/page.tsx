2cc8a999a0244c295722d59c42887b988ef85924
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-23 11:46:55 +0300

    page_get => handled generation qr on get page

--- apps/client/src/app/qr/get/page.tsx
+++ apps/client/src/app/qr/get/page.tsx
@@ -2,15 +2,19 @@
 
 import WrapCSR from "@/common/components/HOC/pageWrappers/WrapCSR";
 import { useWrapQuery } from "@/core/hooks/api/useWrapQuery";
+import QrRes from "@/features/qr/components/QrRes/QrRes";
 import { qrSliceAPI } from "@/features/qr/slices/api";
-import { postQrForm } from "@shared/first/schemas/qr.post.js";
+import { getQrState, qrSlice } from "@/features/qr/slices/slice";
+import { postQrForm, PostQrFormT } from "@shared/first/schemas/qr.post.js";
 import { useSearchParams } from "next/navigation";
-import type { FC } from "react";
+import { useEffect, useMemo, type FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 } from "uuid";
 
 const Page: FC = () => {
   const query = useSearchParams();
 
-  const queryObj = Object.fromEntries(query);
+  const queryObj = useMemo(() => Object.fromEntries(query), [query]);
   const isOk = postQrForm.safeParse(queryObj).success;
 
   if (!isOk) throw new Error("Invalid query data");
@@ -23,6 +27,15 @@ const Page: FC = () => {
   });
   const { isLoading, data: { blob } = {} } = res;
   const isApiOk = blob instanceof Blob;
+  const dispatch = useDispatch();
+  const { urlCode } = useSelector(getQrState);
+
+  useEffect(() => {
+    if (!isApiOk) return;
+
+    dispatch(qrSlice.actions.setQr(URL.createObjectURL(blob)));
+    dispatch(qrSlice.actions.setInput(queryObj as PostQrFormT));
+  }, [isApiOk, dispatch, blob, queryObj]);
 
   return (
     <WrapCSR
@@ -32,7 +45,13 @@ const Page: FC = () => {
         isLoading,
       }}
     >
-      <div className=""></div>
+      <QrRes
+        {...{
+          fileName: `${v4()}.${blob?.type}`,
+          input: queryObj as PostQrFormT,
+          urlCode,
+        }}
+      />
     </WrapCSR>
   );
 };
